caadbc57e84b5c74b5a3ec972cffecec01ef08f3
Rushi Gandhi <rushigandhi@users.noreply.github.com>
2016-09-20 23:09:47 -0400

    Add files via upload

---
+++ DichotomousKey.java
@@ -0,0 +1,50 @@
+import java.util.Scanner;
+
+public class DichotomousKey{
+   public static void main(String[] args){
+      Scanner in = new Scanner(System.in);
+      System.out.println("Input 1 for YES and 0 for NO.");
+      System.out.println("Is the kingdom Eukaryotic?");
+      int a = in.nextInt();
+      if (a == 1){
+      System.out.println("Does the kingdom have locomotion?");
+      int c = in.nextInt();
+      if(c==0){
+      System.out.println("Is the kindgom autotrophic?");
+      int d = in.nextInt();
+      if (d==1){
+            System.out.println("The kingdom is PLANTAE!");
+      }
+      else if(d==0){
+            System.out.println("The kingdom is FUNGI!");
+}
+}
+      else if (c==1){
+            System.out.println("Can the kingdom live on land?");
+            int e = in.nextInt();
+            if(e==1){
+                        System.out.println("The kingdom is ANIMALIA!");
+         }
+     else if(e==0){
+                 System.out.println("The kingdom is PROTISTA!");
+
+      }
+      }
+      }
+      if(a==0){
+      System.out.println("Does the kingdom contain peptidoglycan in its cell wall?");
+      int b = in.nextInt();
+      if (b==1){
+      System.out.println("The kingdom is EUBACTERIA!");
+      }
+      else if (b==0){
+      System.out.println("The kingdom is ARCHAEABACTERIA!");
+      }
+      
+      }
+      }
+      }
+  
+
+            
+      
